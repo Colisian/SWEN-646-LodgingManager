1ea5a600e8709290415a66e0b23f6f215acdb8dd
Colisian McLeod <mcleodom16@gmail.com>
2023-03-01 22:02:49 -0500

    Made changes to Address class.

--- src/main/java/lodging/Address.java
+++ src/main/java/lodging/Address.java
@@ -2,30 +2,45 @@ package lodging;
 
 public class Address {
     private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public Address(String street, String city, String state, String zipCode){
+        //Assign values to attributes
+    }
+
+
+    //Create and return a copy of the object
+    public Address clone() throws CloneNotSupportedException{
+        Address add = new Address(this.street, this.city, this.state,this.zipCode);
+        return add;
+        //return new lodging.Address(this.street, this.city, this.state, this.zipCode)
+        //return (Address) super.clone();
+    }
+    public Address(String line) throws CloneNotSupportedException{ //Creates and returns copy of object with line parameter
+        street = line.substring(line.indexOf("<street>") + 8, line.indexOf("</street>"));
+        city = line.substring(line.indexOf("<city>") + 6, line.indexOf("</city"));
+        state = line.substring(line.indexOf("<state>") + 7, line.indexOf("</state>"));
+        zipCode = line.substring(line.indexOf("<zip>") + 8, line.indexOf("</zip>"));
+
+    }
     public String getStreet() {
         return street;
     }
     public void setStreet(String street) {
         this.street = street;
     }
-
-    private String city;
     public String getCity() {
         return city;
     }
     public void setCity(String city) {
         this.city = city;
     }
-
-    private String state;
-    public String getState() {
-        return state;
-    }
+    public String getState() {return state;}
     public void setState(String state) {
         this.state = state;
     }
-
-    private String zipCode;
     public String getZipCode() {
         return zipCode;
     }
@@ -33,22 +48,13 @@ public class Address {
         this.zipCode = zipCode;
     }
 
-    public Address(String street, String city, String state, String zipCode){
-        //Assign values to attributes
-    }
-
-    //format and return object's data in JSON format
     public String toString(){
         //return
-        return null;
-    }
-
-    //Create and return a copy of the object
-    public Address clone() throws CloneNotSupportedException{
-        //return new lodging.Address(this.street, this.city, this.state, this.zipCode)
-        return (Address) super.clone();
-    }
-    public Address clone(String line) throws CloneNotSupportedException{ //Creates and returns copy of object with line parameter
-        return (Address) super.clone();
+        return "<address>" +
+                "<street>" + street + "</street>" +
+                "<city>" + city + "</city>" +
+                "<state>" + state + "</state>" +
+                "<zip>" + zipCode + "</zip>" +
+                "</address>";
     }
 }
